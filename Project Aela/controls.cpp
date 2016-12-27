8f9868104e06ba9b99dfbc01f53fa0ed4358483a
Robert <bobbyewy@gmail.com>
2016-12-27 17:20:20 -0500

    Window Focus Fixed (Alt-Tabbing doesn't break everything!)
    Changed the behaviour of the engine so that it only performs actions
    related to the keyboard and mouse if the window has focus. Alt-Tabbing
    out of the window will now let the user have control over the mouse! :D

--- Project Aela/controls.cpp
+++ Project Aela/controls.cpp
@@ -42,93 +42,95 @@ clock_t currentTime = clock(), lastTime;
 
 
 void computeMatricesFromInputs(AelaWindow * window) {
-	lastTime = currentTime;
+	if (window->isFocused()) {
+		lastTime = currentTime;
 
-	// Compute time difference between current and last frame
-	currentTime = clock();
-	float deltaTime = float(currentTime - lastTime);
+		// Compute time difference between current and last frame
+		currentTime = clock();
+		float deltaTime = float(currentTime - lastTime);
 
-	if (window->keyPressed(225)) {
-		currentSpeed = superSpeed;
-	} else {
-		currentSpeed = speed;
+		if (window->keyPressed(225)) {
+			currentSpeed = superSpeed;
+		} else {
+			currentSpeed = speed;
+		}
+
+		// Get mouse position
+		int xpos, ypos;
+		window->getCursorPositionInWindow(&xpos, &ypos);
+
+		// Reset mouse position for next frame
+		int width, height;
+		window->getWindowDimensions(&width, &height);
+		window->setCursorPositionInWindow(width / 2, height / 2);
+
+
+		// Compute new orientation
+		horizontalAngle += mouseSpeed * float(width / 2 - xpos);
+		verticalAngle += mouseSpeed * float(height / 2 - ypos);
+
+
+		// Direction : Spherical coordinates to Cartesian coordinates conversion
+		glm::vec3 direction(
+			cos(verticalAngle) * sin(horizontalAngle),
+			sin(verticalAngle),
+			cos(verticalAngle) * cos(horizontalAngle)
+		);
+
+		// Right vector
+		glm::vec3 right = glm::vec3(
+			sin(horizontalAngle - 3.14f / 2.0f),
+			0,
+			cos(horizontalAngle - 3.14f / 2.0f)
+		);
+
+		glm::vec3 straightUp(
+			0,
+			0.5,
+			0
+		);
+
+		// Up vector
+		glm::vec3 up = glm::cross(right, direction);
+
+		// Move forward
+		if (window->keyPressed(26)) {
+			position += direction * deltaTime * currentSpeed;
+		}
+		// Move backward
+		if (window->keyPressed(22)) {
+			position -= direction * deltaTime * currentSpeed;
+		}
+		// Strafe right
+		if (window->keyPressed(7)) {
+			position += right * deltaTime * currentSpeed;
+		}
+		// Strafe left
+		if (window->keyPressed(4)) {
+			position -= right * deltaTime * currentSpeed;
+		}
+
+		if (window->keyPressed(44)) {
+			position += straightUp * deltaTime * currentSpeed;
+		}
+
+		if (window->keyPressed(224)) {
+			position -= straightUp * deltaTime * currentSpeed;
+		}
+
+
+		float FoV = initialFoV;
+
+		// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+		ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
+		// Camera matrix
+		ViewMatrix = glm::lookAt(
+			position,           // Camera is here
+			position + direction, // and looks here : at the same position, plus "direction"
+			up                  // Head is up (set to 0,-1,0 to look upside-down)
+		);
+
+		// For the next frame, the "last time" will be "now"
+		lastTime = currentTime;
 	}
-
-	// Get mouse position
-	int xpos, ypos;
-	window->getCursorPositionInWindow(&xpos, &ypos);
-
-	// Reset mouse position for next frame
-	int width, height;
-	window->getWindowDimensions(&width, &height);
-	window->setCursorPositionInWindow(width / 2, height / 2);
-
-
-	// Compute new orientation
-	horizontalAngle += mouseSpeed * float(width / 2 - xpos);
-	verticalAngle += mouseSpeed * float(height / 2 - ypos);
-
-
-	// Direction : Spherical coordinates to Cartesian coordinates conversion
-	glm::vec3 direction(
-		cos(verticalAngle) * sin(horizontalAngle),
-		sin(verticalAngle),
-		cos(verticalAngle) * cos(horizontalAngle)
-	);
-
-	// Right vector
-	glm::vec3 right = glm::vec3(
-		sin(horizontalAngle - 3.14f / 2.0f),
-		0,
-		cos(horizontalAngle - 3.14f / 2.0f)
-	);
-
-	glm::vec3 straightUp(
-		0,
-		0.5,
-		0
-	);
-
-	// Up vector
-	glm::vec3 up = glm::cross(right, direction);
-
-	// Move forward
-	if (window->keyPressed(26)) {
-		position += direction * deltaTime * currentSpeed;
-	}
-	// Move backward
-	if (window->keyPressed(22)) {
-		position -= direction * deltaTime * currentSpeed;
-	}
-	// Strafe right
-	if (window->keyPressed(7)) {
-		position += right * deltaTime * currentSpeed;
-	}
-	// Strafe left
-	if (window->keyPressed(4)) {
-		position -= right * deltaTime * currentSpeed;
-	}
-
-	if (window->keyPressed(44)) {
-		position += straightUp * deltaTime * currentSpeed;
-	}
-
-	if (window->keyPressed(224)) {
-		position -= straightUp * deltaTime * currentSpeed;
-	}
-
-
-	float FoV = initialFoV;
-
-	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
-	// Camera matrix
-	ViewMatrix = glm::lookAt(
-		position,           // Camera is here
-		position + direction, // and looks here : at the same position, plus "direction"
-		up                  // Head is up (set to 0,-1,0 to look upside-down)
-	);
-
-	// For the next frame, the "last time" will be "now"
-	lastTime = currentTime;
 }
