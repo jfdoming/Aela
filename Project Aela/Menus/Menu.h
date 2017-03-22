38f3aa0fcf3dbd73ace62cc883f942bcb9665ab8
Julian Frank Dominguez-Schatz <dolphinspray.fr@gmail.com>
2017-03-21 21:03:58 -0400

    Menus!

---
+++ Project Aela/Menus/Menu.h
@@ -0,0 +1,15 @@
+/*
+* Class: Menu
+* Author: Julian Dominguez-Schatz
+* Date: 03/20/2017
+* Description: Represents an entire menu in our game. It can contain various
+               containers, widgets and other assorted GUI elements.
+*/
+
+#pragma once
+class Menu : public Container {
+public:
+	Menu();
+	~Menu();
+};
+
