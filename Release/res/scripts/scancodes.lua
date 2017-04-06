8eb2f6f0739b3ead36dc89af78f107addc4d3976
Haxerus <haxerus.corporation@gmail.com>
2017-04-05 23:33:52 -0400

    Properly sync only resources

---
+++ Release/res/scripts/scancodes.lua
@@ -0,0 +1,63 @@
+local keys = {}
+
+--LETTERS
+keys["A"] = 4
+keys["B"] = 5
+keys["C"] = 6
+keys["D"] = 7
+keys["E"] = 8
+keys["F"] = 9
+keys["G"] = 10
+keys["H"] = 11
+keys["I"] = 12
+keys["J"] = 13
+keys["K"] = 14
+keys["L"] = 15
+keys["M"] = 16
+keys["N"] = 17
+keys["O"] = 18
+keys["P"] = 19
+keys["Q"] = 20
+keys["R"] = 21
+keys["S"] = 22
+keys["T"] = 23
+keys["U"] = 24
+keys["V"] = 25
+keys["W"] = 26
+keys["X"] = 27
+keys["Y"] = 28
+keys["Z"] = 29
+
+--NUMBERS
+keys["1"] = 30
+keys["2"] = 31
+keys["3"] = 32
+keys["4"] = 33
+keys["5"] = 34
+keys["6"] = 35
+keys["7"] = 36
+keys["8"] = 37
+keys["9"] = 38
+keys["10"] = 39
+
+--AUXILLARY
+keys["ENTER"] = 40
+keys["ESC"] = 41
+keys["SPACE"] = 44
+keys["SLASH"] = 56
+
+keys["F1"] = 58
+keys["F2"] = 59
+keys["F3"] = 60
+
+keys["SCROLL_LOCK"] = 71
+
+keys["RIGHT"] = 79
+keys["LEFT"] = 80
+keys["DOWN"] = 81
+keys["UP"] = 82
+
+
+
+
+
